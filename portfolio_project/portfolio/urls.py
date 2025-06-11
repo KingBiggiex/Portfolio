46907f396eceb1e8dd030bd4ddf8e0effb7cda9d
Treasure <lottringt12018@gmail.com>
2025-06-11 22:56:20 +0200

    Frontend done

--- portfolio_project/portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('project/<int:project_id>/', views.projecrt_detail, name='project_detail'),
-    path('contact/', views.contact, name='contact'),
-]
