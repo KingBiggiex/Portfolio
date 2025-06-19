6f9a8b4ca9b56ad926e1000413e3a0f35321c247
Treasure <lottringt12018@gmail.com>
2025-06-19 14:12:12 +0200

    refactor: clean up pom.xml and application.properties, remove unused configurations

--- Portfolio-backend/src/main/java/com/example/Portfolio_backend/PortfolioBackendApplication.java
+++ Portfolio-backend/src/main/java/com/example/Portfolio_backend/PortfolioBackendApplication.java
@@ -2,9 +2,6 @@ package com.example.Portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
@@ -13,18 +10,4 @@ public class PortfolioBackendApplication {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
 
-
-
-// Add this to your main Spring Boot class
-@Bean
-public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/**")
-                    .allowedMethods("*")
-                    .allowedOrigins("*");
-        }
-    };
-}
 }
--- Portfolio-backend/src/test/java/com/example/Portfolio_backend/PortfolioBackendApplicationTests.java
+++ Portfolio-backend/src/test/java/com/example/Portfolio_backend/PortfolioBackendApplicationTests.java
@@ -1,23 +1,13 @@
 package com.example.Portfolio_backend;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test") // Use test profile
+@SpringBootTest
 class PortfolioBackendApplicationTests {
-
-	@MockBean
-    private JavaMailSender mailSender;
 
 	@Test
 	void contextLoads() {
-		assertNotNull(mailSender);
 	}
 
 }
