3a3d32d3a66205a30721df8fe997f7709c6218a2
Treasure <lottringt12018@gmail.com>
2025-06-19 17:44:26 +0200

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/Portfolio/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Portfolio"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17255, hash: '3436970a8822c726b7d82ef09a7c23fc7010973090881a51e468563f94517eff', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 16979, hash: '0230a0d4da7fe5561868621bddb222bccf8f22c2581879fc0aa66acc81eaf7db', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 40171, hash: '46dcb1b553e3d6a5de2e29afdda194b7bfa7340d96e94cc6beaa9477c56969c4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 17245, hash: '3484a0f85dc51b0c32d884aed919ff068ff0a54cb43f9ac34309737c13f68ce2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 16969, hash: '6a9f7811be254f69d691ed034893f833903fab0d0af8c8aa26734728071f023c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 40161, hash: 'dbeafe4f4a348cfe98666b0981382028983f4bb2290fbeb1669caa86301606e0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-AZHLLXQH.css': {size: 7050, hash: 'iSZv2ZpiWUY', text: () => import('./assets-chunks/styles-AZHLLXQH_css.mjs').then(m => m.default)}
   },
 };
