ff8b79dee27d6b5702939033a27f4f7ca86b5c91
KingBiggiex <lottringt12018@gmail.com>
2025-07-09 10:21:11 +0200

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'https://KingBiggiex.github.io/Portfolio',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: 'https://KingBiggiex.github.io/Portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/Portfolio"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17245, hash: '3484a0f85dc51b0c32d884aed919ff068ff0a54cb43f9ac34309737c13f68ce2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 16969, hash: '6a9f7811be254f69d691ed034893f833903fab0d0af8c8aa26734728071f023c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 40161, hash: 'dbeafe4f4a348cfe98666b0981382028983f4bb2290fbeb1669caa86301606e0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 17284, hash: '4f8c863e32b3ba9cb085773fc71f04b95430d9e09fd3cd6b7238e08f7de1ca42', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17008, hash: 'a9edb9a8dd44e7994943f87250fc6c99badfe62d7685647cf45755813a7c09a1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 40200, hash: '7da84f25c5f41db9154c5c2ab6dfab365cf002f7606d3daf24192ad6de08c366', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-AZHLLXQH.css': {size: 7050, hash: 'iSZv2ZpiWUY', text: () => import('./assets-chunks/styles-AZHLLXQH_css.mjs').then(m => m.default)}
   },
 };
